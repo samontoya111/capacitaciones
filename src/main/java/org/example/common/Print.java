76b3e1e9b6f734d6fed4c283de2d2bed800e0112
samontoya111 <smontoya111@gmail.com>
2024-05-31 20:07:48 -0500

    .

--- src/main/java/org/example/common/Print.java
+++ src/main/java/org/example/common/Print.java
@@ -5,6 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Print {
     public static void welcome(){
-      log.info("Este es el sistema de Gestion de Empleados");  
+      log.info("Este es el sistema de Gestion de Empleados.");
     }
 }
